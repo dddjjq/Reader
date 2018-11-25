c8e0e97c98e32eabc7a161488981f2f0bf2a84f0
welson <dingyunlong1996@hotmail.com>
2018-11-25 21:55:28 +0800

    daily

--- app/src/main/java/com/welson/reader/view/ReadView.java
+++ app/src/main/java/com/welson/reader/view/ReadView.java
@@ -2,15 +2,61 @@ package com.welson.reader.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.welson.reader.R;
 
 public class ReadView extends RelativeLayout{
 
+    private TextView readChapterText;
+    private TextView readContentText;
+    private TextView readTimeText;
+    private TextView readCurrentChapterItemText;
+
     public ReadView(Context context) {
         super(context);
+        init(context);
     }
 
     public ReadView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context);
+    }
+
+    private void init(Context context){
+        LayoutInflater.from(context).inflate(R.layout.read_view_layout,this,true);
+        readChapterText = findViewById(R.id.read_chapter);
+        readContentText = findViewById(R.id.read_content);
+        readTimeText = findViewById(R.id.read_current_time);
+        readCurrentChapterItemText = findViewById(R.id.read_current_chapter);
+    }
+
+    public void setReadChapterText(String s){
+        readChapterText.setText(s);
+    }
+
+    public void setReadContentText(String s){
+        readContentText.setText(s);
+    }
+
+    public void setReadTimeText(String s){
+        readTimeText.setText(s);
+    }
+
+    public void setReadCurrentChapterItemText(String s){
+        readCurrentChapterItemText.setText(s);
+    }
+
+    public void setContentTextSize(int size){
+        readContentText.setTextSize(size);
+    }
+
+    public int getMaxLine(){
+        int height = readContentText.getHeight();
+        int lineHeight = readContentText.getLineHeight();
+        readContentText.getMaxLines();
+        return height/lineHeight;
     }
 }
